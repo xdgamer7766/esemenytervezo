dc57207b8ea14195abb4d308cc15a041c1aaaac0
Pingvin0 <pingvinmagas@gmail.com>
2020-07-02 21:29:57 +0200

    started work on ui

---
+++ ui.py
@@ -0,0 +1,16 @@
+
+dashes = 30
+print('-'*dashes)
+print('1) Események megtekintése')
+print('2) Esemény hozzáadása')
+print('3) Esemény törlése')
+print('4) Kilépés')
+
+while True:
+    option = input('Választás: ')
+    available_options = ['1','2','3','4']
+    # Ha a kiválasztott opció nincs benne a available_options tömbbe, nem folytatjuk a loop-ot, az elejére megyünk, és ujra megkérdezzük.
+    if option not in available_options:
+        print('Hibás választás!')
+        continue
+
