ca6471fe648d063aeee883e2c70ced0258d8eb4f
xdgamer7766 <oszpolgarbenedek@gmail.com>
2020-07-02 21:16:14 +0200

    class létrehozása

--- class.py
+++ class.py
@@ -1,1 +1,6 @@
-class esemeny 
+class Esemeny:
+    def __init__(self,date,name,address,price):
+        self.date = Esemeny_datum
+        self.name = Esemeny_nev
+        self.address = Esemeny_cim
+        self.price = Esemeny_ar
