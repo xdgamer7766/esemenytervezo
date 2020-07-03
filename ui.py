46bce91eee9bd5b1d3d6d9c0ad9a9b80bc5528b9
Pingvin0 <pingvinmagas@gmail.com>
2020-07-03 16:03:51 +0200

    removed ui main loop

--- ui.py
+++ ui.py
@@ -15,24 +15,3 @@ def getMenuView(items):
     ...
 
 
-
-
-while False:
-    option = input('Választás: ')
-    available_options = ['1','2','3','4']
-    # Ha a kiválasztott opció nincs benne a available_options tömbbe, nem folytatjuk a loop-ot, az elejére megyünk, és ujra megkérdezzük.
-    if option not in available_options:
-        print('Hibás választás!')
-        continue
-    
-    if option == '1':
-        print(getEventDisplayStr())
-        # Todo: Wait for more input, multiple pages, 5 items per page, sorted by date
-    if option == '2':
-        #Todo: add event ui
-        ...
-    if option == '3':
-        #Todo: Get event id and remove based on that.
-        ...
-    if option == '4':
-        break
