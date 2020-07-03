31dc9469c70c7fdaf4afc963f20a0e615a8b5b76
Pingvin0 <pingvinmagas@gmail.com>
2020-07-03 16:04:09 +0200

    removed more code

--- ui.py
+++ ui.py
@@ -1,12 +1,5 @@
 
 dashes = 60
-print('-'*dashes)
-print('1) Események megtekintése')
-print('2) Esemény hozzáadása')
-print('3) Esemény törlése')
-print('4) Kilépés')
-print('-'*dashes)
-
 
 def decorateStr(s):
     ...
